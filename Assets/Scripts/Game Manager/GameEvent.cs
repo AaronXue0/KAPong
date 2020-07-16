1d4f163e3130073e36b3d279e1d8db7af9083763
AaronXue <her8153101@yahoo.com.tw>
2020-07-16 15:54:14 +0800

    Fire Ball Ability Image Shuffle System.
    1. Add GameEvent.cs to control Image Shuffle System.
    2. Add image shuffle UI canvas and image.
    Pending:
        1. Fire Ball abilites.
        2. Health bar effect when player dead.

---
+++ Assets/Scripts/Game Manager/GameEvent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEvent : MonoBehaviour
+{
+    [Header("Fire Ball Ability Events")]
+    [SerializeField]
+    Image abilityBox;
+    [SerializeField]
+    Sprite[] abilitySprite;
+    [SerializeField]
+    float shuffleDuration;
+    [SerializeField]
+    int maxShuffleTime;
+    int shuffleTime = 0;
+
+    int randomAbility;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) Shuffle();
+    }
+
+    void Shuffle()
+    {
+        shuffleTime = maxShuffleTime;
+        StartCoroutine(SetImage(shuffleDuration, 0));
+    }
+    IEnumerator SetImage(float delay, int n)
+    {
+        yield return new WaitForSeconds(delay);
+        if (n < abilitySprite.Length)
+        {
+            abilityBox.sprite = abilitySprite[n];
+            StartCoroutine(SetImage(shuffleDuration, n + 1));
+        }
+        else if (shuffleTime > 0)
+        {
+            shuffleTime--;
+            StartCoroutine(SetImage(shuffleDuration, 0));
+        }
+        else
+        {
+            randomAbility = Random.Range(0, abilitySprite.Length);
+            abilityBox.sprite = abilitySprite[randomAbility];
+        }
+    }
+}
