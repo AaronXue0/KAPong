60661122100355e7fc61d00d45854b7f95c54fd5
AaronXue <her8153101@yahoo.com.tw>
2020-07-26 12:28:05 +0800

    Setup store feature sample.
    1. Add StoreFeatureScene.
    2. Add StoreSystem script.
    3. Add store shake visual effect(prototye).
    
    Hot Fix:
        1. Boder collision glitch.
        2. Aniamtion frame rate optimization.
        3. Swipe menu optimization.
        4. AI control optimization
    Pending:
        1. Fire Ball abilites.
        2. Health bar visiual effect when player dead.

---
+++ Assets/Scripts/Game Manager/StoreSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreSystem : MonoBehaviour
+{
+    public RectTransform content;
+    public Text moneyText;
+    public PlayerItem item;
+
+    public void AddMoney()
+    {
+        item.money += 10;
+        ChangeMoneyView();
+    }
+    public void AddMoney(int cost)
+    {
+        if (item.money + cost < 0)
+        {
+            StartCoroutine(ShakeDelay(0.15f, 0.4f));
+            return;
+        }
+        item.money += cost;
+        ChangeMoneyView();
+    }
+    void Start()
+    {
+        moneyText.text = "Money: " + item.money.ToString();
+    }
+    void ChangeMoneyView()
+    {
+        moneyText.text = "Money: " + item.money.ToString();
+    }
+    IEnumerator ShakeDelay(float duration, float magnitude)
+    {
+        Vector3 originalPos = content.localPosition;
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            content.localPosition = new Vector3(x,y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        content.localPosition = originalPos;
+    }
+}
