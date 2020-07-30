8fa1c32ff8cf02ad9f7b9c632b4535a537136911
AaronXue <her8153101@yahoo.com.tw>
2020-07-30 17:53:51 +0800

    Setup main menu scene.
    1. Add selectmenu script.
    2. Add btns to route to different scene.
    
    Hot Fix:
        1. Boder collision glitch.
        2. Aniamtion frame rate optimization.
        3. Swipe menu optimization.
        4. AI control optimization
    Pending:
        1. Fire Ball abilites.
        2. Health bar visiual effect when player dead.

---
+++ Assets/Scripts/Menu Stuffs/SelectScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SelectScene : MonoBehaviour
+{
+    public void LoadB(int sceneANumber)
+    {
+        SceneManager.LoadScene(sceneANumber);
+    }
+}
