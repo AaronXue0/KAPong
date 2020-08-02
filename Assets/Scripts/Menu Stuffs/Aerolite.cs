ea6d0120fa5e55cd120c643a5bfcb9e33c1e14b3
AaronXue <her8153101@yahoo.com.tw>
2020-08-02 08:16:44 +0800

    Add new background object.
    1. Add aerolite in menu.
    
    Hot Fix:
        1. Boder collision glitch.
        2. Aniamtion frame rate optimization.
        3. Swipe menu optimization.
        4. AI control optimization
    Pending:
        1. Fire Ball abilites.
        2. Health bar visiual effect when player dead.

---
+++ Assets/Scripts/Menu Stuffs/Aerolite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aerolite : MonoBehaviour
+{
+    public float RotateSpeed;
+    private float Radius;
+
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    private void Update()
+    {
+        Radius = Screen.width * 0.3f;
+        _angle += RotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        transform.position = _centre + offset;
+    }
+}
