1cd41d825fece75536423bbc387b22dc65bb390c
AaronXue <her8153101@yahoo.com.tw>
2020-08-03 15:00:30 +0800

    Refactor menu feature.

---
+++ Assets/Scripts/Menu Stuffs/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    Text broadCastText;
+
+    public void LoadScene(int sceneANumber)
+    {
+        SceneManager.LoadScene(sceneANumber);
+    }
+    public void OpenLinking(string url)
+    {
+        Application.OpenURL(url);
+    }
+    public void BroadCast(string news)
+    {
+        broadCastText.text = news;
+    }
+    public void ColoredText(float a)
+    {
+        Color color = broadCastText.color;
+        color.a = a;
+        broadCastText.color = color;
+    }  
+}
--- Assets/Scripts/Menu Stuffs/SelectScene.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-public class SelectScene : MonoBehaviour
-{
-    public void LoadB(int sceneANumber)
-    {
-        SceneManager.LoadScene(sceneANumber);
-    }
-}
