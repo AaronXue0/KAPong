2b73f779bb798cb2cac939f7501a3947eab97bcc
AaronXue <her8153101@yahoo.com.tw>
2020-10-06 15:53:02 +0800

    Menu Refactor.

--- Assets/Scripts/ADs/InitializeAdsScript.cs
+++ Assets/Scripts/ADs/InitializeAdsScript.cs
@@ -3,74 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
+namespace Menuspace
 {
-    #if UNITY_IOS
+    public class InitializeAdsScript : MonoBehaviour, IUnityAdsListener
+    {
+#if UNITY_IOS
     private string gameId = "3796674";
-    #elif UNITY_ANDROID
-    private string gameId = "3796675";
-    #endif
-    string myPlacementId = "rewardedVideo";
-    bool testMode = false;
+#elif UNITY_ANDROID
+        private string gameId = "3796675";
+#endif
+        string myPlacementId = "rewardedVideo";
+        bool testMode = false;
 
-    void Start()
-    {
-        Advertisement.AddListener(this);
-        Advertisement.Initialize(gameId, testMode);
-    }
-
-    public void ShowRewardedVideo()
-    {
-        // Check if UnityAds ready before calling Show method:
-        if (Advertisement.IsReady(myPlacementId))
+        void Start()
         {
-            Advertisement.Show(myPlacementId);
+            Advertisement.AddListener(this);
+            Advertisement.Initialize(gameId, testMode);
         }
-        else
-        {
-            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
-        }
-    }
 
-    // Implement IUnityAdsListener interface methods:
-    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
-    {
-        // Define conditional logic for each ad completion status:
-        if (showResult == ShowResult.Finished)
+        public void ShowRewardedVideo()
         {
-            // Reward the user for watching the ad to completion.
+            // Check if UnityAds ready before calling Show method:
+            if (Advertisement.IsReady(myPlacementId))
+            {
+                Advertisement.Show(myPlacementId);
+            }
+            else
+            {
+                Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+            }
         }
-        else if (showResult == ShowResult.Skipped)
+
+        // Implement IUnityAdsListener interface methods:
+        public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
         {
-            // Do not reward the user for skipping the ad.
+            // Define conditional logic for each ad completion status:
+            if (showResult == ShowResult.Finished)
+            {
+                GetComponent<Menu>().PlayThanksFeedback();
+                Debug.Log("Success");
+            }
+            else if (showResult == ShowResult.Skipped)
+            {
+                // Do not reward the user for skipping the ad.
+                Debug.Log("Failed");
+            }
+            else if (showResult == ShowResult.Failed)
+            {
+                Debug.LogWarning("The ad did not finish due to an error.");
+            }
         }
-        else if (showResult == ShowResult.Failed)
+
+        public void OnUnityAdsReady(string placementId)
         {
-            Debug.LogWarning("The ad did not finish due to an error.");
+            // If the ready Placement is rewarded, show the ad:
+            if (placementId == myPlacementId)
+            {
+            }
         }
-    }
 
-    public void OnUnityAdsReady(string placementId)
-    {
-        // If the ready Placement is rewarded, show the ad:
-        if (placementId == myPlacementId)
+        public void OnUnityAdsDidError(string message)
         {
+            // Log the error.
         }
-    }
 
-    public void OnUnityAdsDidError(string message)
-    {
-        // Log the error.
-    }
+        public void OnUnityAdsDidStart(string placementId)
+        {
+            // Optional actions to take when the end-users triggers an ad.
+        }
 
-    public void OnUnityAdsDidStart(string placementId)
-    {
-        // Optional actions to take when the end-users triggers an ad.
-    }
-
-    // When the object that subscribes to ad events is destroyed, remove the listener:
-    public void OnDestroy()
-    {
-        Advertisement.RemoveListener(this);
+        // When the object that subscribes to ad events is destroyed, remove the listener:
+        public void OnDestroy()
+        {
+            Advertisement.RemoveListener(this);
+        }
     }
 }
--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+﻿// using UnityEngine.VFX;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.VFX;
 using UnityEngine.Playables;
 using DG.Tweening;
 
@@ -11,9 +11,37 @@ namespace Menuspace
 {
     public class Menu : MonoBehaviour
     {
+        public GameObject pausePanel;
+        public GameObject spaceship;
         public PlayableAsset[] clips;
+        public PlayableAsset supportClip;
         PlayableDirector director;
 
+        public bool appFocus;
+        bool isOpeningURL;
+        Vector2 scale;
+
+        void OnGUI()
+        {
+            if (appFocus == false)
+            {
+                pausePanel.SetActive(true);
+            }
+        }
+
+        void OnApplicationFocus(bool focusStatus)
+        {
+            appFocus = focusStatus;
+        }
+        public void AboutUs(GameObject btn)
+        {
+            Application.OpenURL("https://www.facebook.com/Game-Starry-109356847225732/?view_public_for=109356847225732");
+        }
+        public void PlayThanksFeedback()
+        {
+            director.playableAsset = supportClip;
+            director.Play();
+        }
         public void TransitionAToB()
         {
             director.playableAsset = clips[0];
@@ -28,9 +56,12 @@ namespace Menuspace
         {
             director = GetComponent<PlayableDirector>();
         }
-        private void Update()
+        void Update()
         {
-
+            if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+            {
+                if (pausePanel.activeSelf) pausePanel.SetActive(false);
+            }
         }
     }
 
